6dd9bfff8a51119f3a49a91a9e1667ce4ad4a478
iamvikramjeet1910 <chhotu123kota@gmail.com>
2024-02-11 23:03:27 +0530

    Adding entire repo

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Vikram Kumar on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
